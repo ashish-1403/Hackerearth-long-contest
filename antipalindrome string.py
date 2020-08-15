1648511355fc0d1ffe76a4a547d0871b4f5bf709
ashish-1403 <64345992+ashish-1403@users.noreply.github.com>
2020-08-16 04:11:42 +0530

    august circuita =

---
+++ antipalindrome string.py
@@ -0,0 +1,16 @@
+t=int(input())
+for i in range(t):
+    s=list(input())
+    a=s[::-1]
+    if a==s:
+        print(-1)
+    else:
+        s.sort()
+        c=" "
+        s=c.join(s)
+        print(s.replace(" ",""))
+       
+   
+
+
+    
